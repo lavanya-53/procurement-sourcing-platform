594d67fc0aa2671e150e3b2509273238a4e3798f
lavanyaygowda <lavanyay202@gmail.com>
2026-06-28 17:45:44 +0530

    feat: add AttachmentRepository for attachment persistence

---
+++ src/main/java/com/spo/core_app/Repository/AttachmentRepository.java
@@ -0,0 +1,12 @@
+package com.spo.core_app.Repository;
+
+import com.spo.core_app.models.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AttachmentRepository extends JpaRepository<Attachment, UUID> {
+
+}
