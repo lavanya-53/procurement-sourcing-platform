b8756beb03d14dfd24e9d3380f0c8af6854e7be1
lavanyaygowda <lavanyay202@gmail.com>
2026-07-01 22:17:34 +0530

    feat(email): add email constants for notification templates and subject lines

---
+++ src/main/java/com/spo/core_app/Constants/EmailConstants.java
@@ -0,0 +1,6 @@
+package com.spo.core_app.Constants;
+
+public class EmailConstants {
+    public static final String Procurement_Company_Reg_Email_Template_name="Procurement-Company-REgistration-Email";
+    public static final String procurement_Company_email_Subject_Line="Welcome to Procurement Portal";
+}
