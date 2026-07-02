bc2687fe5e915bcc612a5adec9fd43a8e019a164
lavanyaygowda <lavanyay202@gmail.com>
2026-07-02 23:31:52 +0530

    feat(auth): add employee login DTO and authentication endpoint

---
+++ src/main/java/com/spo/core_app/DtoRequests/Empdto.java
@@ -0,0 +1,14 @@
+package com.spo.core_app.DtoRequests;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Empdto {
+    private String email;
+    private String password;
+}
