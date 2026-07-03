4f938e2bae7fe1dc40a9220a46cc3b5b965337f0
lavanyaygowda <lavanyay202@gmail.com>
2026-07-03 20:28:36 +0530

    feat(repository): add ProcurementCompanyRepository for company persistence operations

---
+++ src/main/java/com/spo/core_app/Repository/ProcurementCompanyRepository.java
@@ -0,0 +1,13 @@
+package com.spo.core_app.Repository;
+
+import com.spo.core_app.models.ProcurementCompany;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProcurementCompanyRepository extends JpaRepository<ProcurementCompany, UUID> {
+
+
+}
