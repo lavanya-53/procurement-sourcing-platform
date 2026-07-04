9e30c4075f95e0e1edee5dcf86a28781fd858021
lavanyaygowda <lavanyay202@gmail.com>
2026-07-04 22:29:27 +0530

    refactor(constants): update SystemConstants with additional application configurations

---
+++ src/main/java/com/spo/core_app/Constants/SystemConstants.java
@@ -0,0 +1,16 @@
+package com.spo.core_app.Constants;
+
+public class SystemConstants {
+    public static final String APPLICATION_USER_NAME="System";
+    public static final String AWS_SERVICE_NAME="AWS";
+    public static final String AZURE_SERVICE_NAME="AZURE";
+    public static final String IMAGEKIT_SERVICE_NAME="IMAGEKIT";
+    public static final int File_Upload_retry=3;
+    public static final String procurement_company_basepath="/ProcurementCompany/Registration/docs";
+    public static final String Attachment_entity_name="Attachment";
+    public static final String Employee_Entity_Name ="Employee";
+    public static final String Role_Entity_name="Role";
+    public static final Long JWT_TOKEN_EXPIRATION_TIME = 1_800_000L;
+    public static final String JWT_SECRET_PASSWORD = "bcjevcjevcjevbcvhevcegvhgevchevjvejbudbwcbwjdbwdjxjkwgwgdjwbcjevejcvbejvcevfcvueb";
+}
+
